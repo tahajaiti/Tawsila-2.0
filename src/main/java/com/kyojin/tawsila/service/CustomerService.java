26537e2821ff977150d86535b8b94d3d03e2c967
tahajaiti <taha.jaiti@gmail.com>
2025-11-04 16:00:28 +0100

    FEAT: TS-90 created the customer service interface with jdocs

---
+++ src/main/java/com/kyojin/tawsila/service/CustomerService.java
@@ -0,0 +1,47 @@
+package com.kyojin.tawsila.service;
+
+import com.kyojin.tawsila.dto.CustomerDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerService {
+
+    /**
+     * Create a new customer.
+     *
+     * @param customerDTO the DTO containing customer data
+     * @return the created CustomerDTO
+     */
+    CustomerDTO createCustomer(CustomerDTO customerDTO);
+
+    /**
+     * Get a customer by its ID.
+     *
+     * @param id the ID of the customer
+     * @return an Optional containing the CustomerDTO if found
+     */
+    Optional<CustomerDTO> getCustomerById(Long id);
+
+    /**
+     * Get all customers.
+     *
+     * @return a list of CustomerDTOs
+     */
+    List<CustomerDTO> getAllCustomers();
+
+    /**
+     * Delete a customer by its ID.
+     *
+     * @param id the ID of the customer to delete
+     */
+    void deleteCustomerById(Long id);
+
+    /**
+     * Update an existing customer.
+     *
+     * @param customerDTO the DTO containing updated customer data
+     * @return the updated CustomerDTO
+     */
+    CustomerDTO updateCustomer(CustomerDTO customerDTO);
+}
