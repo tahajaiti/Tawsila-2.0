b4e06c299dbaf842c0e881c6d807c8ff77c818a7
tahajaiti <taha.jaiti@gmail.com>
2025-11-04 15:57:03 +0100

    FEAT: TS-88 created the customer mapper DONE

---
+++ src/main/java/com/kyojin/tawsila/mapper/CustomerMapper.java
@@ -0,0 +1,11 @@
+package com.kyojin.tawsila.mapper;
+
+import com.kyojin.tawsila.dto.CustomerDTO;
+import com.kyojin.tawsila.entity.Customer;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+    Customer toEntity(CustomerDTO customerDTO);
+    CustomerDTO toDTO(Customer customer);
+}
