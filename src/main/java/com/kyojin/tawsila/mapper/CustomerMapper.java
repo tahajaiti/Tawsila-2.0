adddff21f4d106280ec30e12fd941820a0f3a66e
tahajaiti <taha.jaiti@gmail.com>
2025-11-10 17:04:56 +0100

    FIX: fixed a problem withj the customer mapper

--- src/main/java/com/kyojin/tawsila/mapper/CustomerMapper.java
+++ src/main/java/com/kyojin/tawsila/mapper/CustomerMapper.java
@@ -3,11 +3,14 @@ package com.kyojin.tawsila.mapper;
 import com.kyojin.tawsila.dto.CustomerDTO;
 import com.kyojin.tawsila.entity.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
     Customer toEntity(CustomerDTO customerDTO);
     CustomerDTO toDTO(Customer customer);
 
-    void updateEntityFromDTO(CustomerDTO customerDTO, Customer customer);
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDTO(CustomerDTO customerDTO, @MappingTarget Customer customer);
 }
