af6ed9ce396a120b925d460b58c5cbaa50cf1e43
tahajaiti <taha.jaiti@gmail.com>
2025-11-12 10:10:24 +0100

    FEAT: TS-111 created a method in delivery hstory repo to fetch relevant histories DONE

--- src/main/java/com/kyojin/tawsila/repository/DeliveryHistoryRepository.java
+++ src/main/java/com/kyojin/tawsila/repository/DeliveryHistoryRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface DeliveryHistoryRepository extends JpaRepository<DeliveryHistory, Long> {
     boolean existsByDeliveryAndDeliveryDate(Delivery delivery, LocalDate date);
+    List<DeliveryHistory> findTop50ByOrderByDeliveryDateDesc();
 }
