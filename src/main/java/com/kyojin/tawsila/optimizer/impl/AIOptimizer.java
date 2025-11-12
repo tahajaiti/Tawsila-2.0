46332f2660c6f2a839bf324b832752f2ff6e53e0
tahajaiti <taha.jaiti@gmail.com>
2025-11-12 10:09:35 +0100

    FEAT: TS-110 injected the ai client and deliveryhistory repo to the aioptimzer DONE

--- src/main/java/com/kyojin/tawsila/optimizer/impl/AIOptimizer.java
+++ src/main/java/com/kyojin/tawsila/optimizer/impl/AIOptimizer.java
@@ -4,10 +4,25 @@ import com.kyojin.tawsila.entity.Delivery;
 import com.kyojin.tawsila.entity.Vehicle;
 import com.kyojin.tawsila.entity.Warehouse;
 import com.kyojin.tawsila.optimizer.TourOptimizer;
+import com.kyojin.tawsila.repository.DeliveryHistoryRepository;
+import org.springframework.ai.azure.openai.AzureOpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class AIOptimizer implements TourOptimizer {
+
+    private final AzureOpenAiChatModel chatModel;
+    private final DeliveryHistoryRepository deliveryHistoryRepository;
+
+    @Autowired
+    public AIOptimizer(AzureOpenAiChatModel chatModel, DeliveryHistoryRepository deliveryHistoryRepository) {
+        this.chatModel = chatModel;
+        this.deliveryHistoryRepository = deliveryHistoryRepository;
+    }
+
 
     @Override
     public List<Delivery> calculateOptimalTour(Warehouse warehouse, List<Delivery> deliveries, Vehicle vehicle) {
